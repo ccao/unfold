a60ffcdef71cb9e4adb29ccb2c610ce61c1f40ac
ccao <ccao1981@gmail.com>
2018-04-26 16:48:45 +0800

    Changed name. Symmetrize Hamiltonian works

--- util/symmham.c
+++ util/symmham.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <complex.h>
 
 #include "constants.h"
 #include "wanndata.h"
@@ -10,18 +11,22 @@ int main(int argc, char ** argv) {
 
   wanndata in;
   wanndata * out;
+  wanndata final;
   vector * symm;
   vector * site;
   int * info;
   int nwann, nsymm;
+  int mode;
 
   FILE * fin;
   char line[MAXLEN];
   char seed[MAXLEN];
   int ii, jj;
 
-  if (argc<2) {
-    printf("Usage: %s <INPUTFILE>\n", argv[0]);
+  if (argc<3) {
+    printf("Usage: %s <INPUTFILE> MODE\n", argv[0]);
+    printf("   MODE: 0: output Hamiltonian after symmetry operation.\n");
+    printf("         1: symmetrize over the operation.\n");
     exit(0);
   }
 
@@ -51,7 +56,13 @@ int main(int argc, char ** argv) {
 
   fclose(fin);
 
-  printf(" Will generate %5d hamiltonians.\n", nsymm);
+  sscanf(argv[2], " %d", &mode);
+  if (mode==0) {
+    printf(" Will generate %5d hamiltonians.\n", nsymm);
+  }
+  else {
+    printf(" Will symmetrize the Hailtonian.\n");
+  }
 
   read_ham(&in, seed);
   if (nwann!=in.norb) {
@@ -68,12 +79,30 @@ int main(int argc, char ** argv) {
     rotate_ham(out+ii, &in, symm+ii*4, site, info);
   }
 
-  for (ii=0; ii<nsymm; ii++) {
-    sprintf(seed, "output%d", ii+1);
-    write_ham(out+ii, seed);
-    finalize_wanndata(out[ii]);
+  if (mode==0) {
+    for (ii=0; ii<nsymm; ii++) {
+      sprintf(seed, "output%d", ii+1);
+      write_ham(out+ii, seed);
+    }
+  }
+  else {
+    final.nrpt=in.nrpt;
+    final.norb=in.norb;
+    init_wanndata(&final);
+    memcpy(final.rvec, in.rvec, sizeof(vector)*in.nrpt);
+    memcpy(final.weight, in.weight, sizeof(int)*in.nrpt);
+    for (ii=0; ii<in.nrpt*in.norb*in.norb; ii++) {
+      final.ham[ii]=0.0;
+      for (jj=0; jj<nsymm; jj++) {
+        final.ham[ii]+=out[jj].ham[ii];
+      }
+      final.ham[ii]/=nsymm;
+    }
+    write_ham(&final, "output");
+    finalize_wanndata(final);
   }
 
+  for (ii=0; ii<nsymm; ii++) finalize_wanndata(out[ii]);
   finalize_wanndata(in);
   free(symm);
   free(site);
